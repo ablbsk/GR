a3e9ac0e2347262411d53c0870cfa86febf4c283
Aliaksei Balabushka <aliaksei.balabushka@gmail.com>
2019-11-06 15:22:02 +0200

    Copy app.get

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ app.post('/api/auth', (req, res) => {
   });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static( 'client/build' ));
 
