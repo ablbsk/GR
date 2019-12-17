c7525ebebe1d4899c3f1b3c4eed886f5a68328ab
Aliaksei Balabushka <aliaksei.balabushka@gmail.com>
2019-12-17 17:45:27 +0200

    Moved server files

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.post('/api/auth', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname + '../client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, + '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
